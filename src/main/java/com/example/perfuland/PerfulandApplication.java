33525856bfb0dd673fa3b8816cb65357a7ab97e0
Andru Alarcon <idazhdz@gmail.com>
2025-05-19 12:54:04 -0400

    Proyecto inicial

---
+++ src/main/java/com/example/perfuland/PerfulandApplication.java
@@ -0,0 +1,13 @@
+package com.example.perfuland;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerfulandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerfulandApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/perfuland/PerfulandApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.perfuland;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerfulandApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
